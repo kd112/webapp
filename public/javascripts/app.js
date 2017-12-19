4d161667596540bc7bd52475c3ada28c7982d723
Rahul <rahul@Rahuls-Mac.local>
2017-12-19 16:51:46 +0800

    modified map div width

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,14 @@
 // var L 	= require('leaflet')
 
 $(document).ready(function () {
-    var map = L.map( 'map-canvas', { center: [-31.95, 115.86], zoom: 11 } );
+    var map = L.map( 'map-canvas', {zoomControl:false, center: [-31.95, 115.86], zoom: 11 } );
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+    
+    L.control.zoom({
+     position:'bottomright'
+	}).addTo(map);
 
 	});
 
