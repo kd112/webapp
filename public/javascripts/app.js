b13366be7ed4ddba6258c55b5f14238e832704c1
Rahul <rahul@Rahuls-Mac.local>
2018-01-20 11:17:10 +0800

    commiting for github

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 // var L 	= require('leaflet')
 
 $(document).ready(function () {
-    var map = L.map( 'map-canvas', {zoomControl:false, center: [-31.95, 115.86], zoom: 11 } );
+    var map = L.map( 'map-canvas', {zoomControl:false, center: [22.2970760, 73.1957373], zoom: 11 } );
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
