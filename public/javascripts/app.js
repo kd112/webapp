14b525cea168e8a2478c34614c397c5eb90c248f
Rahul <rahul@Rahuls-Mac.local>
2018-02-01 21:45:28 +0800

    Added url to geoserver

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,12 @@
 // var L 	= require('leaflet')
 
 $(document).ready(function () {
-    var map = L.map( 'map-canvas', {zoomControl:false, center: [22.2970760, 73.1957373], zoom: 11 } );
+    var map = L.map( 'map-canvas', {zoomControl:false, center: [21.99, 77.99], zoom: 5 } );
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
+    L.tileLayer("http://localhost:8080/geoserver/wms?bbox=58.10,5.06,103.19,37.24&Format=image/png&request=GetMap&layers=India:State&width=256&height=256&srs=EPSG:4326&TRANSPARENT=TRUE&tiled=true").addTo(map);
     L.control.zoom({
      position:'bottomright'
 	}).addTo(map);
